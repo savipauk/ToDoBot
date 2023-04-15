63db56edf3e39d16f3396412babb61b93bd61795
savipauk <erik.kranjec8@outlook.com>
2023-04-16 01:17:11 +0200

    Setup bot project, added very basic functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+
+const TOKEN = fs.readFileSync("token.txt").toString();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.DirectMessages
+    ]
+});
+
+client.on("ready", () => {
+    console.log(`ready ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+    if (message.content.startsWith("/todo ")) {
+        let asignee = message.author
+        let taskName = message.content.substring(6);
+
+        message.reply(`Created task "${taskName}" for ${asignee}`);
+    }
+});
+
+client.login(TOKEN);
