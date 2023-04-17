da755003373425cf05492790a8e05e9db4542335
Famaso13 <karlo.vlah13@gmail.com>
2023-04-17 20:16:49 +0200

    Added ability to remove tasks from list

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ A discord bot we are making in order to help us track progress on a planned proj
    
   
 ## Funtions:
-/todo - Adds a task to the list (task, Author's Discord ID)    
-/display - prints a list of all tasks and their authors
+/todo - Adds a task to the list \
+&emsp;example: /todo task    
+/display - prints a list of all tasks and their authors\
+&emsp;example: /display
+/remove - removes the selected element from the list based on the id\
+&emsp;example: /remove 1 (removes the element with the id of "1")  
--- index.js
+++ index.js
@@ -28,22 +28,50 @@ client.on("messageCreate", (message) => {
         id++
         let asignee = message.author;
         let taskName = message.content.substring(6);
-        
-        array.table.push({id: id, task: taskName, asignee: asignee.id});
-        let json = JSON.stringify(array);
+        let json;
+        if(id==1){
+            array.table.push({id: id, task: taskName, asignee: asignee.id});
+            json = JSON.stringify(array);
+        }
+        else{
+            let obj_t = fs.readFileSync("tasks.json");
+            obj_t = JSON.parse(obj_t);
+            obj_t.table.push({id: id, task: taskName, asignee: asignee.id});
+            json = JSON.stringify(obj_t);
+        }
 
         message.reply(`Created task "${taskName}" for ${asignee}`);
         fs.writeFileSync("tasks.json", json, {flag:"w"});
     }
 
     else if (message.content.startsWith("/display")) {
-        let obj = fs.readFileSync("tasks.json");
-        obj = JSON.parse(obj);
+        let obj_d = fs.readFileSync("tasks.json");
+        obj_d = JSON.parse(obj_d);
         let reply = "";
-        obj.table.forEach(element => {
+        obj_d.table.forEach(element => {
             reply += `${element.id}. ${element.task}, by <@${element.asignee}> \n`
            // message.reply(`${element.id}. ${element.taskName}, by <@${element.asignee   }>`); 
         });
+        message.reply(reply);
+    }
+
+    else if (message.content.startsWith("/remove ")) {
+        let content = message.content.substring(8);
+        let ids = content.split(" ");
+        let reply = "";
+        let obj_rm = fs.readFileSync("tasks.json");
+        obj_rm = JSON.parse(obj_rm);
+        ids.forEach(id => {
+            console.log(id);
+            obj_rm.table.forEach(element => {
+                if(id == element.id){
+                    reply += `Deleted task: ${element.task}\n`
+                    obj_rm.table.splice(element.id-1,1);
+                }
+            });
+        });
+        obj_rm = JSON.stringify(obj_rm);
+        fs.writeFileSync("tasks.json", obj_rm, {flag:"w"});
         message.reply(reply);
     }
 });
