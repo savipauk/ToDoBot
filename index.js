6a1e32bef05d6adfcdff1838d2731f800cacbf88
Erik Kranjec <erik.kranjec8@outlook.com>
2023-04-18 18:33:08 +0200

    Set up command handling, setup /todo command

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
+const path = require('path');
 
-const TOKEN = fs.readFileSync("token.txt").toString();
+const config = require('./config.json');
+
+const TOKEN = config.token;
 
 const client = new Client({
     intents: [
@@ -13,11 +16,53 @@ const client = new Client({
     ]
 });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath); // gets the module.export
+
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName); // gets value of key (module.exp)
+
+    if (!command) {
+        console.error(`no command matching ${interaction.commandName} found`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+
+    console.log(interaction);
+});
+
+
 let id = 0;
 let array = {
     table: []
- };
-fs.writeFileSync("tasks.json", "", {flag:"w"});
+};
+fs.writeFileSync("tasks.json", "", { flag: "w" });
 
 client.on("ready", () => {
     console.log(`ready ${client.user.tag}`);
@@ -29,19 +74,19 @@ client.on("messageCreate", (message) => {
         let asignee = message.author;
         let taskName = message.content.substring(6);
         let json;
-        if(id==1){
-            array.table.push({id: id, task: taskName, asignee: asignee.id});
+        if (id == 1) {
+            array.table.push({ id: id, task: taskName, asignee: asignee.id });
             json = JSON.stringify(array);
         }
-        else{
+        else {
             let obj_t = fs.readFileSync("tasks.json");
             obj_t = JSON.parse(obj_t);
-            obj_t.table.push({id: id, task: taskName, asignee: asignee.id});
+            obj_t.table.push({ id: id, task: taskName, asignee: asignee.id });
             json = JSON.stringify(obj_t);
         }
 
         message.reply(`Created task "${taskName}" for ${asignee}`);
-        fs.writeFileSync("tasks.json", json, {flag:"w"});
+        fs.writeFileSync("tasks.json", json, { flag: "w" });
     }
 
     else if (message.content.startsWith("/display")) {
@@ -50,7 +95,7 @@ client.on("messageCreate", (message) => {
         let reply = "";
         obj_d.table.forEach(element => {
             reply += `${element.id}. ${element.task}, by <@${element.asignee}> \n`
-           // message.reply(`${element.id}. ${element.taskName}, by <@${element.asignee   }>`); 
+            // message.reply(`${element.id}. ${element.taskName}, by <@${element.asignee   }>`); 
         });
         message.reply(reply);
     }
@@ -64,14 +109,14 @@ client.on("messageCreate", (message) => {
         ids.forEach(id => {
             console.log(id);
             obj_rm.table.forEach(element => {
-                if(id == element.id){
+                if (id == element.id) {
                     reply += `Deleted task: ${element.task}\n`
-                    obj_rm.table.splice(element.id-1,1);
+                    obj_rm.table.splice(element.id - 1, 1);
                 }
             });
         });
         obj_rm = JSON.stringify(obj_rm);
-        fs.writeFileSync("tasks.json", obj_rm, {flag:"w"});
+        fs.writeFileSync("tasks.json", obj_rm, { flag: "w" });
         message.reply(reply);
     }
 });
