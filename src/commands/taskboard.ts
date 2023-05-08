094848ac97e20a160b8e259019574e0f1d313ca4
Erik Kranjec <erik.kranjec8@outlook.com>
2023-05-08 13:43:49 +0200

    extracted GetTaskboardTextChannel to be a standalone function

--- src/commands/taskboard.ts
+++ src/commands/taskboard.ts
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 import { AnyThreadChannel, ApplicationCommandOptionType, ApplicationCommandType, Channel, ChannelType, CommandInteraction, Guild, TextChannel } from 'discord.js';
-import { threadId } from 'worker_threads';
 import { GetTasks, SetThreadId, Task, TasksToString } from '../tasks';
 import { Command } from '../types/Command';
 import { ToDoClient } from '../types/ToDoClient';
@@ -20,8 +19,6 @@ export const Taskboard: Command = {
     ],
     run: async (interaction: CommandInteraction, client: ToDoClient) => {
         let channelID = interaction.options.get('channel').value.toString();
-
-        // console.log(channelID);
 
         let channel: Channel = await client.channels.fetch(channelID);
 
@@ -50,7 +47,7 @@ export const Taskboard: Command = {
 }
 
 export async function CreateThreadForTask(task: Task, client: ToDoClient): Promise<string> {
-    let channel: TextChannel = await client.channels.fetch(client.taskboardID) as TextChannel;
+    let channel: TextChannel = await GetTaskboardTextChannel(client);
 
     let taskThread = await channel.threads.create({
         name: `${task.id} | ${task.description} | <@${task.assignee}>`,
@@ -63,12 +60,16 @@ export async function CreateThreadForTask(task: Task, client: ToDoClient): Promi
 }
 
 export async function CloseThreadForTask(task: Task, client: ToDoClient): Promise<AnyThreadChannel<boolean>> {
-    let channel: TextChannel = await client.channels.fetch(client.taskboardID) as TextChannel;
+    let channel: TextChannel = await GetTaskboardTextChannel(client);
 
     let thread = await channel.threads.fetch(task.threadId);
     await thread.send("Task remove. Closing thread.");
 
     return await thread.setArchived();
+}
+
+async function GetTaskboardTextChannel(client: ToDoClient): Promise<TextChannel> {
+    return await client.channels.fetch(client.taskboardID) as TextChannel;
 }
 
 export async function SendMessageToThread(task: Task, client: ToDoClient): Promise<string> {
